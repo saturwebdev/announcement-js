50b7d3f2b3ad87fccc8836793d6440007b990849
saturwebdev <122088976+saturwebdev@users.noreply.github.com>
2023-01-12 02:02:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,73 @@
-function getData() {
+let announcementForm = document.getElementById("announcementForm")
+let date = document.getElementById("date")
+let title = document.getElementById("title")
+let body = document.getElementById("body")
+let imageUrl = document.getElementById("imageUrl")
+let createdBy = document.getElementById("createdBy")
+let submitButton = document.getElementById("submitButton")
+let buttonSpinner = document.getElementById("buttonSpinner")
+let buttonText = document.getElementById("buttonText")
+let unknownError = document.getElementById("unknownError")
+
+function afterSubmit(e) {
+  e.preventDefault()
+
+  if (announcementForm.checkValidity() === false) {
+    e.stopPropagation()
+    for (field of announcementForm.elements) {
+      if (!field.checkValidity()) {
+        field.classList.add("is-invalid")
+      } else {
+        field.classList.remove("is-invalid")
+      }
+    }
+    return
+  }
+
+  for (field of announcementForm.elements) {
+    field.classList.remove("is-invalid")
+  }
+
+  let info = {
+    date: date.value,
+    title: title.value,
+    body: body.value,
+    image: imageUrl.value,
+    createdby: createdBy.value
+  }
+
   const url = "https://script.google.com/macros/s/AKfycbz1rgNZo7NMzHbDmaBcD9_Sgqz1cIsgZdKeonXdO9DnmG0b8xGwcNFh54F8wEEQEzGCEw/exec"
 
-  fetch(url)
-    .then(response => response.json())
-    .then(response => {
-      document.getElementById("app").textContent = response[0].status
-    })
-}
-
-function setData() {
-  const url = "https://script.google.com/macros/s/AKfycbz1rgNZo7NMzHbDmaBcD9_Sgqz1cIsgZdKeonXdO9DnmG0b8xGwcNFh54F8wEEQEzGCEw/exec"
+  buttonText.textContent = "Saving.."
+  buttonSpinner.classList.remove("d-none")
+  submitButton.disabled = true
 
   fetch(url, {
     method: "POST",
-    mode: "no-cors",
     cache: "no-cache",
-    credentials: "omit",
-    headers: {
-      "Content-Type": "application/json"
-    },
     redirect: "follow",
-    body: JSON.stringify({"title":"Generate your eRCS (LOA) in a few click!","date":"2022-12-17T16:00:00.000Z","body":"","image":"https://drive.google.com/file/d/1J2YIZgLCVQ3h4amrw34gsDEp6IuFJ1ac/view?usp=share_link","createdby":"Admin"})
+    body: JSON.stringify(info)
   })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      announcementForm.reset()
+      buttonText.textContent = "Send"
+      buttonSpinner.classList.add("d-none")
+      submitButton.disabled = false
+    })
+    .catch(err => {
+      console.log(err)
+      console.log("Something went wrong")
+      unknownError.classList.remove("d-none")
 
-  console.log("setData")
+      setTimeout(() => {
+        unknownError.classList.add("d-none")
+        buttonText.textContent = "Send"
+        buttonSpinner.classList.add("d-none")
+        submitButton.disabled = false
+      }, 3000)
+    })
 }
 
-document.getElementById("btn").addEventListener("click", getData)
-document.getElementById("btn2").addEventListener("click", setData)
+announcementForm.addEventListener("submit", afterSubmit)
